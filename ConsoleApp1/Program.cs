4ce0a78d4bbedef53326376d9772334b904396c0
Mohammed Omran <Mohammed Omran@DESKTOP-1AQ0Q85>
2021-10-24 23:01:56 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var list = new DoubleLinkedList();
+            list.InsertLast(list, 7);
+            list.InsertLast(list, 9);
+            list.InsertLast(list, 11);
+            list.PrintList(list);
+            list.DeleteNode(list, 7);
+            Console.WriteLine("after deleting");
+            list.PrintList(list);
+
+        }
+    }
+}
